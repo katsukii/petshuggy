3b731ffc1594f5c60cf93fe639d9b175c5d0497f
Yusuke Katsuki <katsuki.yusuke@gmail.com>
2018-06-19 21:44:59 +0900

    ユーザー編集導線の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 	root :to => "pages#index"
 
-	devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
+	devise_for :users, controllers: { :omniauth_callbacks => 'users/omniauth_callbacks', :registrations => 'registrations' }
 	resources :users, only: [:show]
 end
